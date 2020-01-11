1deb73d81e7791d200e2b88ece73904b5d2f3324
13840272115 <59674995+13840272115@users.noreply.github.com>
2020-01-11 16:23:59 +0800

    Update DataClientLevelMapper.java
    编辑DataClientLevelMapper
    初始创建了全查询、增删改方法

--- src/main/java/com/neusoft/crm/mapper/basdxtl/DataClientLevelMapper.java
+++ src/main/java/com/neusoft/crm/mapper/basdxtl/DataClientLevelMapper.java
@@ -1,5 +1,27 @@
 package com.neusoft.crm.mapper.basdxtl;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import com.neusoft.crm.entity.DataClientLevel;
+
+@Mapper
 public interface DataClientLevelMapper {
 
+	@Insert("insert into dataClientLevel(null,#{dclName})")
+	public int insertDataClientLevel(DataClientLevel dataClientLevel);
+
+	@Delete("delete from dataClientLevel where dclId=#{dclId}")
+	public int deleteDataClientLevel(DataClientLevel dataClientLevel);
+
+	@Update("update dataClientLevel set dclName=#{dclName} where dclId=#{dclId}")
+	public int updateNewsType(DataClientLevel dataClientLevel);
+
+	@Select("select * from dataClientLevel order by dclId")
+	public List<DataClientLevel> selectDataClientLevelAll();
 }
