afe38d824a6c852e60d1f944d9f2a18833431476
13840272115 <59674995+13840272115@users.noreply.github.com>
2020-01-11 16:22:32 +0800

    Create DataClientLevelController.java
    客户等级数据控制层
    初始创建了全查询、增删改方法

---
+++ src/main/java/com/neusoft/crm/controller/basdxtl/DataClientLevelController.java
@@ -0,0 +1,36 @@
+package com.neusoft.crm.controller.basdxtl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.neusoft.crm.entity.DataClientLevel;
+import com.neusoft.crm.service.basdxtl.DataClientLevelService;
+
+@RestController
+public class DataClientLevelController {
+
+	@Autowired
+	private DataClientLevelService dataClientLevelService;
+
+	@RequestMapping("/insertDataClientLevel")
+	public int insertDataClientLevel(@RequestBody DataClientLevel dataClientLevel) throws Exception {
+		return dataClientLevelService.insertDataClientLevel(dataClientLevel);
+	}
+	@RequestMapping("/deleteDataClientLevel")
+	public int deleteDataClientLevel(@RequestBody DataClientLevel dataClientLevel) throws Exception {
+		return dataClientLevelService.deleteDataClientLevel(dataClientLevel);
+	}
+	@RequestMapping("/updateDataClientLevel")
+    public int updateDataClientLevel(@RequestBody DataClientLevel dataClientLevel) throws Exception {
+		return dataClientLevelService.updateNewsType(dataClientLevel);
+}
+	@RequestMapping("selectDataClientLevelAll")
+	public List<DataClientLevel> selectDataClientLevelAll() throws Exception{
+        return dataClientLevelService.selectDataClientLevelAll();
+}
+	
+}
