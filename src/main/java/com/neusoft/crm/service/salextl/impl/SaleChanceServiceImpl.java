1aedd797e5b8d5cc07eb354950a204c1b8c62681
XTL <59634973+ManYI0806@users.noreply.github.com>
2020-01-13 20:16:44 +0800

    Create SaleChanceServiceImpl.java
    编写销售机会服务实现类，实现服务接口中ById查询，分页功能，指派，编辑和删除

---
+++ src/main/java/com/neusoft/crm/service/salextl/impl/SaleChanceServiceImpl.java
@@ -0,0 +1,53 @@
+package com.neusoft.crm.service.salextl.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.neusoft.crm.entity.SaleChance;
+import com.neusoft.crm.mapper.salextl.SaleChanceMapper;
+import com.neusoft.crm.service.salextl.SaleChanceService;
+
+@Service
+public class SaleChanceServiceImpl implements SaleChanceService {
+
+	@Autowired
+	private SaleChanceMapper saleChanceMapper;
+
+	@Override
+	public SaleChance selectSaleChanceById(SaleChance saleChance) {
+		return saleChanceMapper.selectSaleChanceById(saleChance);
+	}
+	
+	@Override
+	public int dueSaleChance(SaleChance saleChance) {
+		return saleChanceMapper.dueSaleChance(saleChance);
+	}
+	
+	@Override
+	public int selectSaleChanceCount(SaleChance saleChance) {
+		return saleChanceMapper.selectSaleChanceCount(saleChance);
+	}
+
+	@Override
+	public List<SaleChance> selectSaleChancePaging(SaleChance saleChance) {
+		return saleChanceMapper.selectSaleChancePaging(saleChance);
+	}
+	
+	@Override
+	public int insertSaleChance(SaleChance saleChance) {
+		return saleChanceMapper.insertSaleChance(saleChance);
+	}
+	
+	@Override
+	public int deleteSaleChanceById(SaleChance saleChance) {
+		return saleChanceMapper.deleteSaleChanceById(saleChance);
+	}
+	
+	@Override
+	public int editSaleChance(SaleChance saleChance) {
+		return saleChanceMapper.editSaleChance(saleChance);
+	}
+	
+}
