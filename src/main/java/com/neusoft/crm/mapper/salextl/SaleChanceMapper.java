557b90e60881ebf6b58be81a6b238a94dc4df56a
XTL <59634973+ManYI0806@users.noreply.github.com>
2020-01-13 20:19:00 +0800

    Update SaleChanceMapper.java
    编写销售机会Mapper接口
    在接口中兼用注解形式新增ById查询，分页功能，指派，编制和删除

--- src/main/java/com/neusoft/crm/mapper/salextl/SaleChanceMapper.java
+++ src/main/java/com/neusoft/crm/mapper/salextl/SaleChanceMapper.java
@@ -1,5 +1,33 @@
 package com.neusoft.crm.mapper.salextl;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import com.neusoft.crm.entity.SaleChance;
+
+@Mapper
 public interface SaleChanceMapper {
 
+	@Select("select * from SaleChance where chanceId=#{chanceId}")
+	public SaleChance selectSaleChanceById(SaleChance saleChance);
+	
+	//模糊查询分页的记录数
+	public int selectSaleChanceCount(SaleChance saleChance);
+	
+	//模糊查询的分页的结果集
+	public List<SaleChance> selectSaleChancePaging(SaleChance saleChance);
+	
+	public int insertSaleChance(SaleChance saleChance);
+
+	@Update("update SaleChance set chanceDueId=#{chanceDueId},chanceDueDate=#{chanceDueDate},chanceStatus=1 where chanceId=#{chanceId}")
+	public int dueSaleChance(SaleChance saleChance);
+	
+	public int editSaleChance(SaleChance saleChance);
+	
+	@Delete("delete from SaleChance where chanceId=#{chanceId}")
+	public int deleteSaleChanceById(SaleChance saleChance);
 }
