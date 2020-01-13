429ab7a9763f4fda45db937c8379b30048f443d6
XTL <59634973+ManYI0806@users.noreply.github.com>
2020-01-13 20:21:03 +0800

    Update SysUserService.java
    修改系统用户服务接口层
    新增加ById查询，查询所有客户经理的方法

--- src/main/java/com/neusoft/crm/service/basdxtl/SysUserService.java
+++ src/main/java/com/neusoft/crm/service/basdxtl/SysUserService.java
@@ -1,9 +1,14 @@
 package com.neusoft.crm.service.basdxtl;
 
+import java.util.List;
+
 import com.neusoft.crm.entity.SysUser;
 
 public interface SysUserService {
 
 	public SysUser selectSysUserByNameByPass(SysUser sysUser);
 	
+	public SysUser selectSysUserById(SysUser sysUser);
+
+	public List<SysUser> selectSysUserCustMgrAll();
 }
