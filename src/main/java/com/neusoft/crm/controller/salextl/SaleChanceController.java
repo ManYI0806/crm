f6423ec8308dd5bd671d807d13673481d394a7cc
XTL <59634973+ManYI0806@users.noreply.github.com>
2020-01-13 20:10:19 +0800

    Create SaleChanceController.java
    编写销售机会控制层代码
    实现销售机会的ById查询，
    分页功能查询，新建，指派，编辑和删除销售机会的功能

---
+++ src/main/java/com/neusoft/crm/controller/salextl/SaleChanceController.java
@@ -0,0 +1,56 @@
+package com.neusoft.crm.controller.salextl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.neusoft.crm.entity.SaleChance;
+import com.neusoft.crm.service.salextl.SaleChanceService;
+
+@RestController
+public class SaleChanceController {
+
+	@Autowired
+	private SaleChanceService saleChanceService;
+
+	@RequestMapping("/selectSaleChanceById")
+	public SaleChance selectSaleChanceById(@RequestBody SaleChance saleChance) throws Exception{
+		return saleChanceService.selectSaleChanceById(saleChance);
+	}
+	
+	@RequestMapping("/selectSaleChanceCount")
+	public int selectSaleChanceCount(@RequestBody SaleChance saleChance) throws Exception {
+		System.out.println(saleChance);
+		return saleChanceService.selectSaleChanceCount(saleChance);
+	}
+
+	@RequestMapping("/selectSaleChancePaging")
+	public List<SaleChance> selectSaleChancePaging(@RequestBody SaleChance saleChance) throws Exception {
+		System.out.println(saleChance);
+		return saleChanceService.selectSaleChancePaging(saleChance);
+	}
+	
+	@RequestMapping("/insertSaleChance")
+	public int insertSaleChance(@RequestBody SaleChance saleChance) throws Exception{
+		return saleChanceService.insertSaleChance(saleChance);
+	}
+	
+	@RequestMapping("/dueSaleChance")
+	public int dueSaleChance(@RequestBody SaleChance saleChance) throws Exception{
+		return saleChanceService.dueSaleChance(saleChance);
+	}
+	
+	@RequestMapping("/editSaleChance")
+	public int editSaleChance(@RequestBody SaleChance saleChance) throws Exception{
+		return saleChanceService.editSaleChance(saleChance);
+	}
+	
+	@RequestMapping("/deleteSaleChanceById")
+	public int deleteSaleChanceById(@RequestBody SaleChance saleChance) throws Exception{
+		return saleChanceService.deleteSaleChanceById(saleChance);
+	}
+	
+}
