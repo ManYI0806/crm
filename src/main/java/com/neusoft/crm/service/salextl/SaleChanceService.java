b16fb4a99607c6c823c7320e59de093a878aafc1
XTL <59634973+ManYI0806@users.noreply.github.com>
2020-01-13 20:15:18 +0800

    Create SaleChanceService.java
    编写销售机会的服务接口层方法
    ById查询，分页功能的实现，新增，指派，编辑和删除的功能实现

---
+++ src/main/java/com/neusoft/crm/service/salextl/SaleChanceService.java
@@ -0,0 +1,23 @@
+package com.neusoft.crm.service.salextl;
+
+import java.util.List;
+
+import com.neusoft.crm.entity.SaleChance;
+
+public interface SaleChanceService {
+	
+	public SaleChance selectSaleChanceById(SaleChance saleChance);
+	
+	public int selectSaleChanceCount(SaleChance saleChance);
+
+	public List<SaleChance> selectSaleChancePaging(SaleChance saleChance);
+	
+	public int insertSaleChance(SaleChance saleChance);
+	
+	public int dueSaleChance(SaleChance saleChance);
+	
+	public int editSaleChance(SaleChance saleChance);
+	
+	public int deleteSaleChanceById(SaleChance saleChance);
+	
+}
