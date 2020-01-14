c163d2f864d1ea398a6d7e75bb6047660910c06a
jianghuan515 <59636865+jianghuan515@users.noreply.github.com>
2020-01-14 09:55:34 +0800

    Create ClientServerServiceImpl.java

---
+++ src/main/java/com/neusoft/crm/service/serverxtl/impl/ClientServerServiceImpl.java
@@ -0,0 +1,23 @@
+package com.neusoft.crm.service.serverxtl.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import com.neusoft.crm.entity.ClientServer;
+import com.neusoft.crm.mapper.serverxtl.ClientServerMapper;
+import com.neusoft.crm.service.serverxtl.ClientServerService;
+
+
+@Service("serverClientServerService")
+public class ClientServerServiceImpl implements ClientServerService {
+    
+	@Autowired
+	@Qualifier("serverClientServerMapper")
+	private ClientServerMapper clientServerMapper;
+
+	@Override
+	public int insertClientServer(ClientServer clientServer) {
+		return clientServerMapper.insertClientServer(clientServer);
+	}
+}
