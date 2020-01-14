e5ce69a992fea2767a606f6ae7f4a95cede5f5a3
13840272115 <59674995+13840272115@users.noreply.github.com>
2020-01-14 10:20:42 +0800

    Update DataClientLevelService.java
    编写了DataClientLevelService.java
    添加了各种方法

--- src/main/java/com/neusoft/crm/service/basdxtl/DataClientLevelService.java
+++ src/main/java/com/neusoft/crm/service/basdxtl/DataClientLevelService.java
@@ -7,7 +7,9 @@ import com.neusoft.crm.entity.DataClientLevel;
 public interface DataClientLevelService {
                
 	public int insertDataClientLevel(DataClientLevel dataClientLevel);
-	public int deleteDataClientLevel(DataClientLevel dataClientLevel);
-	public int updateNewsType(DataClientLevel dataClientLevel);
+	public int deleteDataClientLevelById(DataClientLevel dataClientLevel);
+	public int updateDataClientLevel(DataClientLevel dataClientLevel);
 	public List<DataClientLevel> selectDataClientLevelAll();
+	public DataClientLevel selectDataClientLevelById(DataClientLevel dataClientLevel);
+	public int deleteDataClientLevelBatch(int [] dclIdArr); 
 }
