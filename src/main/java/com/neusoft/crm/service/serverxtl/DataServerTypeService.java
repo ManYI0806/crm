c5d8d1b75a3cee0245ccdcf17e4de870209ec41e
jianghuan515 <59636865+jianghuan515@users.noreply.github.com>
2020-01-14 09:55:22 +0800

    Create DataServerTypeService.java

---
+++ src/main/java/com/neusoft/crm/service/serverxtl/DataServerTypeService.java
@@ -0,0 +1,12 @@
+package com.neusoft.crm.service.serverxtl;
+
+import java.util.List;
+
+import com.neusoft.crm.entity.DataServerType;
+
+
+public interface DataServerTypeService {
+    
+	public List<DataServerType> selectDataServerTypeAll();
+	
+}
