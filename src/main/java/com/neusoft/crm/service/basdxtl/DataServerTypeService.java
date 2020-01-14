4e99b3ef9bf3a35f3bf4b75a907289380e54befa
13840272115 <59674995+13840272115@users.noreply.github.com>
2020-01-14 10:20:26 +0800

    Create DataServerTypeService.java
    编写了DataServerTypeService.java
    添加了各种方法

---
+++ src/main/java/com/neusoft/crm/service/basdxtl/DataServerTypeService.java
@@ -0,0 +1,14 @@
+package com.neusoft.crm.service.basdxtl;
+
+import java.util.List;
+
+import com.neusoft.crm.entity.DataServerType;
+
+public interface DataServerTypeService {
+	public int insertDataServerType(DataServerType dataServerType);
+	public int deleteDataServerTypeById(DataServerType dataServerType);
+	public int updateDataServerType(DataServerType dataServerType);
+	public List<DataServerType> selectDataServerTypeAll();
+	public DataServerType selectDataServerTypeById(DataServerType dataServerType);
+	public int deleteDataServerTypeBatch(int [] dstIdArr); 
+}
