622d6b19fa605a82ed41543d712b370cf21d9794
jianghuan515 <59636865+jianghuan515@users.noreply.github.com>
2020-01-14 09:53:34 +0800

    Create ClientInfoMapper.java

---
+++ src/main/java/com/neusoft/crm/mapper/serverxtl/ClientInfoMapper.java
@@ -0,0 +1,19 @@
+package com.neusoft.crm.mapper.serverxtl;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
+
+import com.neusoft.crm.entity.ClientInfo;
+
+
+@Mapper
+@Repository("serverClientInfoMapper")
+public interface ClientInfoMapper {
+    
+	@Select("select * from clientInfo order by clientCode")
+	public List<ClientInfo> selectClientInfoAll();
+	
+}
