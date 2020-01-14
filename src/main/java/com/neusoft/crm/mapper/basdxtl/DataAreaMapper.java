11894252adbb31856dbf6940795cb4b74d0ef7c2
13840272115 <59674995+13840272115@users.noreply.github.com>
2020-01-14 10:23:00 +0800

    Update DataAreaMapper.java
    编写了DataAreaMapper.java
    添加了各种方法

--- src/main/java/com/neusoft/crm/mapper/basdxtl/DataAreaMapper.java
+++ src/main/java/com/neusoft/crm/mapper/basdxtl/DataAreaMapper.java
@@ -1,5 +1,35 @@
 package com.neusoft.crm.mapper.basdxtl;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import com.neusoft.crm.entity.DataArea;
+
 public interface DataAreaMapper {
+	@Mapper
+	public interface DataClientLevelMapper {
+
+		@Insert("insert into dataArea values(null,#{daName})")
+		public int insertDataArea(DataArea dataArea);
+
+		@Delete("delete from dataArea where daId=#{daId}")
+		public int deleteDataArea(DataArea dataArea);
+
+		@Update("update dataArea set daName=#{daName} where daId=#{daId}")
+		public int updateDataArea(DataArea dataArea);
+
+		@Select("select * from dataArea order by daId")
+		public List<DataArea> selectDataAreaAll();
+
+		@Select("select * from dataArea where daId=#{daId}")
+		public DataArea selectDataAreaById(DataArea dataArea);
+
+		public int deleteDataAreaBatch(int[] daIdArr);
+	}
 
 }
