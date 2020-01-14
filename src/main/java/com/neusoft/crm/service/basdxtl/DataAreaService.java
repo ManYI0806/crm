ab4dd5c1e63908271f2948cbd308ed102d45729a
13840272115 <59674995+13840272115@users.noreply.github.com>
2020-01-14 10:21:20 +0800

    Create DataAreaService.java
    编写了DataAreaService.java文件
    添加了各种方法

---
+++ src/main/java/com/neusoft/crm/service/basdxtl/DataAreaService.java
@@ -0,0 +1,16 @@
+package com.neusoft.crm.service.basdxtl;
+
+import java.util.List;
+
+import com.neusoft.crm.entity.DataArea;
+
+public interface DataAreaService {
+	public int insertDataArea(DataArea dataArea);
+	public int deleteDataArea(DataArea dataArea);
+	public int updateDataArea(DataArea dataArea);
+	public List<DataArea> selectDataAreaAll();
+	public DataArea selectDataAreaById(DataArea dataArea);
+	public int deleteDataAreaBatch(int[] daIdArr);
+	
+	
+}
