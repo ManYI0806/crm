894fbf329e3d8b374274034c7d62ebfea68b6a5b
13840272115 <59674995+13840272115@users.noreply.github.com>
2020-01-14 10:21:52 +0800

    Update DataServerTypeMapper.java
    编写了DataServerTypeMapper.java文件
    添加了各种方法

--- src/main/java/com/neusoft/crm/mapper/basdxtl/DataServerTypeMapper.java
+++ src/main/java/com/neusoft/crm/mapper/basdxtl/DataServerTypeMapper.java
@@ -1,5 +1,31 @@
 package com.neusoft.crm.mapper.basdxtl;
 
-public interface DataServerTypeMapper {
+import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import com.neusoft.crm.entity.DataServerType;
+
+@Mapper
+public interface DataServerTypeMapper {
+	@Insert("insert into dataServerType values(null,#{dstName})")
+	public int insertDataServerType(DataServerType dataServerType);
+
+	@Delete("delete from dataServerType where dstId=#{dstId}")
+	public int deleteDataServerTypeById(DataServerType dataServerType);
+
+	@Update("update dataServerType set dstName=#{dstName} where dstId=#{dstId}")
+	public int updateDataServerType(DataServerType dataServerType);
+
+	@Select("select * from dataServerType order by dstId")
+	public List<DataServerType> selectDataServerTypeAll();
+
+	@Select("select * from dataServerType where dstId=#{dstId}")
+	public DataServerType selectDataServerTypeById(DataServerType dataServerType);
+
+	public int deleteDataServerTypeBatch(int[] dstIdArr);
 }
