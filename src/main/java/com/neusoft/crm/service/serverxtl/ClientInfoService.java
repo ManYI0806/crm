64caf05b1f7c8697b6a428e0e4a67910a35411f0
jianghuan515 <59636865+jianghuan515@users.noreply.github.com>
2020-01-14 09:55:02 +0800

    Create ClientInfoService.java

---
+++ src/main/java/com/neusoft/crm/service/serverxtl/ClientInfoService.java
@@ -0,0 +1,12 @@
+package com.neusoft.crm.service.serverxtl;
+
+import java.util.List;
+
+import com.neusoft.crm.entity.ClientInfo;
+
+
+public interface ClientInfoService {
+
+	public List<ClientInfo> selectClientInfoAll();
+	
+}
