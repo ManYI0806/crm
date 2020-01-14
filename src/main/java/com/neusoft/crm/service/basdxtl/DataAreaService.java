330f579cf6999e0c351906e435cff0afb834a399
13840272115 <59674995+13840272115@users.noreply.github.com>
2020-01-14 11:36:18 +0800

    Update DataAreaService.java
    编写了DataAreaService文件

--- src/main/java/com/neusoft/crm/service/basdxtl/DataAreaService.java
+++ src/main/java/com/neusoft/crm/service/basdxtl/DataAreaService.java
@@ -5,12 +5,11 @@ import java.util.List;
 import com.neusoft.crm.entity.DataArea;
 
 public interface DataAreaService {
+	
 	public int insertDataArea(DataArea dataArea);
 	public int deleteDataArea(DataArea dataArea);
 	public int updateDataArea(DataArea dataArea);
 	public List<DataArea> selectDataAreaAll();
 	public DataArea selectDataAreaById(DataArea dataArea);
 	public int deleteDataAreaBatch(int[] daIdArr);
-	
-	
 }
