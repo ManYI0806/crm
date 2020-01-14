31a3b9c7790c2d9260c739ce8bb4ae3bc507923d
13840272115 <59674995+13840272115@users.noreply.github.com>
2020-01-14 10:23:26 +0800

    Create DataServerTypeController.java
    编写了DataServerTypeController.java文件
    添加了各种方法

---
+++ src/main/java/com/neusoft/crm/controller/basdxtl/DataServerTypeController.java
@@ -0,0 +1,48 @@
+package com.neusoft.crm.controller.basdxtl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.neusoft.crm.entity.DataServerType;
+import com.neusoft.crm.service.basdxtl.DataServerTypeService;
+
+@RestController
+public class DataServerTypeController {
+
+	@Autowired
+	private DataServerTypeService dataServerTypeService;
+
+	@RequestMapping("/insertDataServerType")
+	public int insertDataServerType(@RequestBody DataServerType dataServerType) throws Exception {
+		return dataServerTypeService.insertDataServerType(dataServerType);
+	}
+
+	@RequestMapping("/deleteDataServerType")
+	public int deleteDataServerType(@RequestBody DataServerType dataServerType) throws Exception {
+		return dataServerTypeService.deleteDataServerTypeById(dataServerType);
+	}
+
+	@RequestMapping("/updateDataServerType")
+	public int updateDataServerType(@RequestBody DataServerType dataServerType) throws Exception {
+		return dataServerTypeService.updateDataServerType(dataServerType);
+	}
+
+	@RequestMapping("selectDataServerTypeAll")
+	public List<DataServerType> selectDataServerTypeAll() throws Exception {
+		return dataServerTypeService.selectDataServerTypeAll();
+	}
+
+	@RequestMapping("/selectDataServerTypeById")
+	public DataServerType selectDataServerTypeById(@RequestBody DataServerType dataServerType) throws Exception {
+		return dataServerTypeService.selectDataServerTypeById(dataServerType);
+	}
+
+	@RequestMapping("/deleteDataServerTypeBatch")
+	public int deleteDataServerTypeBatch(@RequestBody int[] dstIdArr) throws Exception {
+		return dataServerTypeService.deleteDataServerTypeBatch(dstIdArr);
+	}
+}
