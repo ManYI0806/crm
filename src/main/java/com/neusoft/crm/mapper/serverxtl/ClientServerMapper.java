fa81b05b5d616333f6a895525e74da1637ef8694
jianghuan515 <59636865+jianghuan515@users.noreply.github.com>
2020-01-14 09:54:14 +0800

    Update ClientServerMapper.java

--- src/main/java/com/neusoft/crm/mapper/serverxtl/ClientServerMapper.java
+++ src/main/java/com/neusoft/crm/mapper/serverxtl/ClientServerMapper.java
@@ -1,5 +1,13 @@
 package com.neusoft.crm.mapper.serverxtl;
 
-public interface ClientServerMapper {
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
+import com.neusoft.crm.entity.ClientServer;
+
+@Mapper
+@Repository("serverClientServerMapper")
+public interface ClientServerMapper {
+	
+	public int insertClientServer(ClientServer clientServer);
 }
