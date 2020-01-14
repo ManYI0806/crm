c1d3d98a2c37e94f18be7fcc5632ffc99ba89002
jianghuan515 <59636865+jianghuan515@users.noreply.github.com>
2020-01-14 09:55:06 +0800

    Create ClientServerService.java

---
+++ src/main/java/com/neusoft/crm/service/serverxtl/ClientServerService.java
@@ -0,0 +1,8 @@
+package com.neusoft.crm.service.serverxtl;
+
+import com.neusoft.crm.entity.ClientServer;
+
+public interface ClientServerService {
+    
+	public int insertClientServer(ClientServer clientServer);
+}
