eee68fcbd5c1199381a3d01081d4fcc8d46fe4ae
13840272115 <59674995+13840272115@users.noreply.github.com>
2020-01-14 10:22:49 +0800

    Update DataClientLevelMapper.java
    编写了DataClientLevelMapper.java
    添加了各种方法

--- src/main/java/com/neusoft/crm/mapper/basdxtl/DataClientLevelMapper.java
+++ src/main/java/com/neusoft/crm/mapper/basdxtl/DataClientLevelMapper.java
@@ -13,15 +13,20 @@ import com.neusoft.crm.entity.DataClientLevel;
 @Mapper
 public interface DataClientLevelMapper {
 
-	@Insert("insert into dataClientLevel(null,#{dclName})")
+	@Insert("insert into dataClientLevel values(null,#{dclName})")
 	public int insertDataClientLevel(DataClientLevel dataClientLevel);
 
 	@Delete("delete from dataClientLevel where dclId=#{dclId}")
-	public int deleteDataClientLevel(DataClientLevel dataClientLevel);
+	public int deleteDataClientLevelById(DataClientLevel dataClientLevel);
 
 	@Update("update dataClientLevel set dclName=#{dclName} where dclId=#{dclId}")
-	public int updateNewsType(DataClientLevel dataClientLevel);
+	public int updateDataClientLevel(DataClientLevel dataClientLevel);
 
 	@Select("select * from dataClientLevel order by dclId")
 	public List<DataClientLevel> selectDataClientLevelAll();
+
+	@Select("select * from dataClientLevel where dclId=#{dclId}")
+	public DataClientLevel selectDataClientLevelById(DataClientLevel dataClientLevel);
+	
+	public int deleteDataClientLevelBatch(int [] dclIdArr); 
 }
