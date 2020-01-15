3a5079a082314ae578280869f0ed5e7ff6d8eafd
jianghuan515 <59636865+jianghuan515@users.noreply.github.com>
2020-01-15 20:18:49 +0800

    Update SysUserService.java

--- src/main/java/com/neusoft/crm/service/basdxtl/SysUserService.java
+++ src/main/java/com/neusoft/crm/service/basdxtl/SysUserService.java
@@ -7,8 +7,10 @@ import com.neusoft.crm.entity.SysUser;
 public interface SysUserService {
 
 	public SysUser selectSysUserByNameByPass(SysUser sysUser);
-	
+
 	public SysUser selectSysUserById(SysUser sysUser);
 
 	public List<SysUser> selectSysUserCustMgrAll();
+
+	public List<SysUser> selectSysUserAll();
 }
