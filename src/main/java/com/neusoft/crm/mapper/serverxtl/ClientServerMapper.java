cd7f56178cf5c93727cf2562d983af3fea6a85e7
jianghuan515 <59636865+jianghuan515@users.noreply.github.com>
2020-01-15 20:19:20 +0800

    Update ClientServerMapper.java

--- src/main/java/com/neusoft/crm/mapper/serverxtl/ClientServerMapper.java
+++ src/main/java/com/neusoft/crm/mapper/serverxtl/ClientServerMapper.java
@@ -1,6 +1,11 @@
 package com.neusoft.crm.mapper.serverxtl;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 import org.springframework.stereotype.Repository;
 
 import com.neusoft.crm.entity.ClientServer;
@@ -10,4 +15,23 @@ import com.neusoft.crm.entity.ClientServer;
 public interface ClientServerMapper {
 	
 	public int insertClientServer(ClientServer clientServer);
+public List<ClientServer> selectClientServerAll();
+	
+	//模糊查询客户服务表
+	public List<ClientServer> selectClientServerByTerm(ClientServer clientServer);
+	
+	//分配
+	@Update("update clientServer set serverDueId=#{serverDueId} where serverId=#{serverId} ")
+	public int updateClientServerByAllot(ClientServer clientServer);
+	
+	//删除
+	@Delete("delete from clientServer where serverId=#{serverId}")
+	public int deleteClientServer(ClientServer clientServer);
+	
+	//查询clientServer的数据总数
+    @Select("select count(*) from clientServer")
+    public int selectClientServerCount();
+    
+    //服务处理第二个全查询界面
+    public List<ClientServer> selectClientServerAllInfo();
 }
