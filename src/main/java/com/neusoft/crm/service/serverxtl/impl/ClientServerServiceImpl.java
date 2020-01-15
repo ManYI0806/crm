4d867771309a15df4d1e6c761b6a0db6e86abbbb
jianghuan515 <59636865+jianghuan515@users.noreply.github.com>
2020-01-15 20:18:20 +0800

    Update ClientServerServiceImpl.java

--- src/main/java/com/neusoft/crm/service/serverxtl/impl/ClientServerServiceImpl.java
+++ src/main/java/com/neusoft/crm/service/serverxtl/impl/ClientServerServiceImpl.java
@@ -1,5 +1,7 @@
 package com.neusoft.crm.service.serverxtl.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -19,5 +21,41 @@ public class ClientServerServiceImpl implements ClientServerService {
 	@Override
 	public int insertClientServer(ClientServer clientServer) {
 		return clientServerMapper.insertClientServer(clientServer);
+	}
+
+	@Override
+	public List<ClientServer> selectClientServerAll() {
+		// TODO Auto-generated method stub
+		return clientServerMapper.selectClientServerAll();
+	}
+
+	@Override
+	public List<ClientServer> selectClientServerByTerm(ClientServer clientServer) {
+		// TODO Auto-generated method stub
+		return clientServerMapper.selectClientServerByTerm(clientServer);
+	}
+
+	@Override
+	public int updateClientServerByAllot(ClientServer clientServer) {
+		// TODO Auto-generated method stub
+		return clientServerMapper.updateClientServerByAllot(clientServer);
+	}
+
+	@Override
+	public int deleteClientServer(ClientServer clientServer) {
+		// TODO Auto-generated method stub
+		return clientServerMapper.deleteClientServer(clientServer);
+	}
+
+	@Override
+	public int selectClientServerCount() {
+		// TODO Auto-generated method stub
+		return clientServerMapper.selectClientServerCount();
+	}
+
+	@Override
+	public List<ClientServer> selectClientServerAllInfo() {
+		// TODO Auto-generated method stub
+		return clientServerMapper.selectClientServerAllInfo();
 	}
 }
