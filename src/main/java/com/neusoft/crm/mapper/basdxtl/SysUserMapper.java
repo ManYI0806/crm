d598963c241dc48e2aa8c97110cb007d43da63e8
13840272115 <59674995+13840272115@users.noreply.github.com>
2020-01-15 17:12:51 +0800

    Update SysUserMapper.java
    编写了SysUserMapper文件

--- src/main/java/com/neusoft/crm/mapper/basdxtl/SysUserMapper.java
+++ src/main/java/com/neusoft/crm/mapper/basdxtl/SysUserMapper.java
@@ -2,8 +2,11 @@ package com.neusoft.crm.mapper.basdxtl;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import com.neusoft.crm.entity.SysUser;
 
@@ -12,10 +15,29 @@ public interface SysUserMapper {
 
 	@Select("select * from sysuser where userName=#{userName} and userPassword=#{userPassword}")
 	public SysUser selectSysUserByNameByPass(SysUser sysUser);
-	
+
 	@Select("select * from sysuser where userId=#{userId}")
 	public SysUser selectSysUserById(SysUser sysUser);
-	
+
 	@Select("select * from sysuser where userRoleId=3")
 	public List<SysUser> selectSysUserCustMgrAll();
+
+	public List<SysUser> selectSysUserAll();
+
+	public int selectSysUserCount(SysUser sysUser);
+
+	public List<SysUser> selectSysUserPaging(SysUser sysUser);
+
+	public int deleteSysUserBatch(int[] sysUserArr);
+	
+	@Update("update SysUser set userName=#{userName},userPassword=#{userPassword},userRoleId=#{userRoleId},daId=#{daId} where userId=#{userId}")
+	public int updateSysUser(SysUser sysUser);
+	
+	@Delete("delete from SysUser where userId=#{userId}")
+	public int deleteSysUserById(SysUser sysUser);
+	
+	@Insert("insert into SysUser (userId,userName,userPassword,userRoleId,daId) values(null,#{userName},1234,#{userRoleId},#{daId})")
+	public int insertSysUser(SysUser sysUser);
+	
+	
 }
