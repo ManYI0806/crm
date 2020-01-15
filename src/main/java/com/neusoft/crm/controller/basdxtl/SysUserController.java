63adb8dc36bbb4078937002e8d4baf1d72852d26
13840272115 <59674995+13840272115@users.noreply.github.com>
2020-01-15 17:13:14 +0800

    Update SysUserController.java
    编写了SysUserController文件

--- src/main/java/com/neusoft/crm/controller/basdxtl/SysUserController.java
+++ src/main/java/com/neusoft/crm/controller/basdxtl/SysUserController.java
@@ -30,4 +30,41 @@ public class SysUserController {
 	public List<SysUser> selectSysUserCustMgrAll() throws Exception{
 		return sysUserService.selectSysUserCustMgrAll();
 	}
+	
+	@RequestMapping("/selectSysUserAll")
+	public List<SysUser> selectSysUserAll() throws Exception{
+		return sysUserService.selectSysUserAll();
+	}
+	
+	@RequestMapping("/selectSysUserCount")
+	public int selectSysUserCount(@RequestBody SysUser sysUser) throws Exception{
+		return sysUserService.selectSysUserCount(sysUser);
+	}
+	
+	@RequestMapping("/selectSysUserPaging")
+	public List<SysUser> selectSysUserPaging(@RequestBody SysUser sysUser) throws Exception{
+		System.out.println(sysUser.getUserName());
+		return sysUserService.selectSysUserPaging(sysUser);
+	}
+	
+	@RequestMapping("/deleteSysUserBatch")
+	public int deleteSysUserBatch(@RequestBody int[] sysUserArr) throws Exception{
+		return sysUserService.deleteSysUserBatch(sysUserArr);
+	}
+	
+	@RequestMapping("/updateSysUser")
+	public int updateSysUser(@RequestBody SysUser sysUser) throws Exception{
+		return sysUserService.updateSysUser(sysUser);
+	}
+	
+	@RequestMapping("/deleteSysUserById")
+	public int deleteSysUserById(@RequestBody SysUser sysUser) throws Exception{
+		return sysUserService.deleteSysUserById(sysUser);
+	}
+	
+	@RequestMapping("/insertSysUser")
+	public int insertSysUser(@RequestBody SysUser sysUser) throws Exception{
+		return sysUserService.insertSysUser(sysUser);
+	}
+
 }
