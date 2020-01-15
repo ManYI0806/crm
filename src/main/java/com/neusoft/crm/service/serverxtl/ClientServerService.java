e7b638a0b6c0978043dc167a3cee8a91eaa6d9db
jianghuan515 <59636865+jianghuan515@users.noreply.github.com>
2020-01-15 20:18:42 +0800

    Update ClientServerService.java

--- src/main/java/com/neusoft/crm/service/serverxtl/ClientServerService.java
+++ src/main/java/com/neusoft/crm/service/serverxtl/ClientServerService.java
@@ -1,8 +1,22 @@
 package com.neusoft.crm.service.serverxtl;
 
+import java.util.List;
+
 import com.neusoft.crm.entity.ClientServer;
 
 public interface ClientServerService {
-    
+
 	public int insertClientServer(ClientServer clientServer);
+
+	public List<ClientServer> selectClientServerAll();
+
+	public List<ClientServer> selectClientServerByTerm(ClientServer clientServer);
+
+	public int updateClientServerByAllot(ClientServer clientServer);
+
+	public int deleteClientServer(ClientServer clientServer);
+
+	public int selectClientServerCount();
+
+	public List<ClientServer> selectClientServerAllInfo();
 }
