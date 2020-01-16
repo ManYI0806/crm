ca7b21f1cfa2760b0ea4e7b0ee7617c6c43c3ee7
13840272115 <59674995+13840272115@users.noreply.github.com>
2020-01-16 12:51:52 +0800

    Update ClientServerServiceImpl.java

--- src/main/java/com/neusoft/crm/service/serverxtl/impl/ClientServerServiceImpl.java
+++ src/main/java/com/neusoft/crm/service/serverxtl/impl/ClientServerServiceImpl.java
@@ -1,5 +1,7 @@
 package com.neusoft.crm.service.serverxtl.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -8,10 +10,9 @@ import com.neusoft.crm.entity.ClientServer;
 import com.neusoft.crm.mapper.serverxtl.ClientServerMapper;
 import com.neusoft.crm.service.serverxtl.ClientServerService;
 
-
 @Service("serverClientServerService")
 public class ClientServerServiceImpl implements ClientServerService {
-    
+
 	@Autowired
 	@Qualifier("serverClientServerMapper")
 	private ClientServerMapper clientServerMapper;
@@ -19,5 +20,11 @@ public class ClientServerServiceImpl implements ClientServerService {
 	@Override
 	public int insertClientServer(ClientServer clientServer) {
 		return clientServerMapper.insertClientServer(clientServer);
+	}
+
+	// rept 
+	@Override
+	public List<ClientServer> selectClientInfoAndClientServerAll() {
+		return clientServerMapper.selectClientInfoAndClientServerAll();
 	}
 }
