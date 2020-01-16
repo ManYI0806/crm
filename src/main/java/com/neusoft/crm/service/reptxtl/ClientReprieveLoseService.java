aef059629dc4412694ea3112a2b3e55f4dfe01ea
13840272115 <59674995+13840272115@users.noreply.github.com>
2020-01-16 12:52:06 +0800

    Create ClientReprieveLoseService.java

---
+++ src/main/java/com/neusoft/crm/service/reptxtl/ClientReprieveLoseService.java
@@ -0,0 +1,12 @@
+package com.neusoft.crm.service.reptxtl;
+
+import java.util.List;
+
+import com.neusoft.crm.entity.ClientReprieveLose;
+
+
+
+public interface ClientReprieveLoseService {
+
+	public List<ClientReprieveLose> selectClientReprieveLoseAll();
+}
