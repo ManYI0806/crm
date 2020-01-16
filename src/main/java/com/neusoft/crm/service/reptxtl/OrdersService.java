8f555ac4cbe627d69d0e47c5027b74312f8d6f59
13840272115 <59674995+13840272115@users.noreply.github.com>
2020-01-16 12:51:58 +0800

    Create OrdersService.java

---
+++ src/main/java/com/neusoft/crm/service/reptxtl/OrdersService.java
@@ -0,0 +1,15 @@
+package com.neusoft.crm.service.reptxtl;
+
+import java.util.List;
+
+import com.neusoft.crm.entity.Orders;
+
+public interface OrdersService {
+	
+	public List<Orders> selectClientInfoAndOrdersAll();
+	
+	/*//下面两个方法专用于分页
+	public int selectOrdersCount(Orders orders);
+	public List<Orders> selectOrdersPaging(Orders orders);*/
+
+}
