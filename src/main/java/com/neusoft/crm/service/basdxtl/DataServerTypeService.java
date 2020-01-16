51d817a22c7618f39643d3e0911761dacce78f0b
13840272115 <59674995+13840272115@users.noreply.github.com>
2020-01-16 12:52:34 +0800

    Update DataServerTypeService.java

--- src/main/java/com/neusoft/crm/service/basdxtl/DataServerTypeService.java
+++ src/main/java/com/neusoft/crm/service/basdxtl/DataServerTypeService.java
@@ -2,6 +2,7 @@ package com.neusoft.crm.service.basdxtl;
 
 import java.util.List;
 
+import com.neusoft.crm.entity.ClientServer;
 import com.neusoft.crm.entity.DataServerType;
 
 public interface DataServerTypeService {
@@ -11,4 +12,6 @@ public interface DataServerTypeService {
 	public List<DataServerType> selectDataServerTypeAll();
 	public DataServerType selectDataServerTypeById(DataServerType dataServerType);
 	public int deleteDataServerTypeBatch(int [] dstIdArr); 
+	//	rept
+	public List<ClientServer> selectDataServerTypeAndClientServerAll();
 }
